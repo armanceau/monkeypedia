5a4a4252e071a3374df6c160080cdb47129242be
Arthur Manceau <102350268+armanceau@users.noreply.github.com>
2025-03-30 14:12:42 +0200

    feat(): Liste_singe & refacto api (ajout de plus de singe)

--- my-app/src/api.ts
+++ my-app/src/api.ts
@@ -1,21 +1,42 @@
 export const fetchMonkeys = async () => {
-  const apiKey = import.meta.env.VITE_API_KEY;
-  const apiUrl = `https://api.api-ninjas.com/v1/animals?name=monkey`;
-
-  try {
-    const response = await fetch(apiUrl, {
-      headers: { "X-Api-Key": apiKey },
-    });
-
-    if (!response.ok) {
-      throw new Error(
-        `Erreur API: ${response.status} - ${response.statusText}`
-      );
+    const apiKey = import.meta.env.VITE_API_KEY;
+    const apiUrl = `https://api.api-ninjas.com/v1/animals`;
+  
+    // Liste des primates
+    const primatesNames = [
+      "chimpanzee",
+      "monkey",
+      "bonobo",
+      "capuchin",
+      "gorilla",
+      "macaque"
+    ];
+  
+    const primates: any[] = [];
+  
+    // On va appeler l'API pour chaque primate de la liste
+    try {
+      for (const name of primatesNames) {
+        const response = await fetch(`${apiUrl}?name=${name}`, {
+          headers: { "X-Api-Key": apiKey },
+        });
+  
+        if (!response.ok) {
+          throw new Error(
+            `Erreur API: ${response.status} - ${response.statusText}`
+          );
+        }
+  
+        const data = await response.json();
+        
+        // Ajouter les données du primate au tableau des primates
+        primates.push(...data);
+      }
+  
+      return primates;
+    } catch (error) {
+      console.error("Erreur lors du chargement des primates :", error);
+      return null;
     }
-
-    return await response.json();
-  } catch (error) {
-    console.error("Erreur lors du chargement des singes :", error);
-    return null;
-  }
-};
+  };
+  
